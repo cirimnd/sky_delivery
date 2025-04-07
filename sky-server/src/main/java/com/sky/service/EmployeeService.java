62d56a8484bc0076cd69a3d68fc303bb5601aaf1
cirimnd <1324448697@qq.com>
2025-04-07 15:55:55 +0800

    启用禁用员工业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -22,4 +22,6 @@ public interface EmployeeService {
     void save(EmployeeDTO employDTO);
 
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void startOrStop(Integer status, Long id);
 }
