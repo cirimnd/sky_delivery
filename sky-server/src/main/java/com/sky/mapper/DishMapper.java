fc8779f9fe2ea79388015ed10799f87bfe0556c3
cirimnd <1324448697@qq.com>
2025-04-17 21:29:43 +0800

    工作台功能实现

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -87,4 +88,11 @@ public interface DishMapper {
      * @return
      */
     List<Dish> getBySetMealId(Long SetmealId);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
